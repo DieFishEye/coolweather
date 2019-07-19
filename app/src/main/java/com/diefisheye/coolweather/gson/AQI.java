3945a62bcc73da44f72ae98f886257a052e38cbb
zxd <3232094813@qq.com>
2019-07-19 15:46:11 +0800

    加入显示天气信息的功能。

---
+++ app/src/main/java/com/diefisheye/coolweather/gson/AQI.java
@@ -0,0 +1,9 @@
+package com.diefisheye.coolweather.gson;
+
+public class AQI {
+    public AQICity city;
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
